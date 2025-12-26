8c55d85cd83b7307ccb0207b826ee192c6ab415d
Delta Student <srushtij58@gmail.com>
2025-12-26 14:38:59 +0530

    updated

---
+++ data.js
@@ -0,0 +1,5 @@
+const cravings = [
+  {name:"Chocolate", recipe:"Cucumber + Sugar → Watermelon feel", calories:150, meme:"You don’t want chocolate, you want peace."},
+  {name:"Pizza", recipe:"Roasted oats → Chips vibe", calories:180, meme:"Pizza? Nah, peace is better."},
+  {name:"Ice Cream", recipe:"Frozen banana ice-cream", calories:120, meme:"Cold swap for hot cravings!"}
+];
